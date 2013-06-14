9e6a9b6d00206fcc73d2d09f1dd3cbbe15aeecf9
William Huey <wklhuey@gmail.com>
2013-06-13 22:18:51 -0700

    preliminary collapse for sidebar items

---
+++ public/js/jquery-collapse.js
@@ -0,0 +1,38 @@
+var jQuery = $ = require("component-jquery");
+
+$(document).ready(function () {
+
+  var $siblings1 = $('a.mod-link').parent().parent().siblings();
+  $siblings1.slideUp();
+
+  $('#nav-list').on('click', function (event) {
+    event.preventDefault();
+
+    var target = event.target,
+      targetTag = target.tagName.toLowerCase(),
+      $target = $(target),
+      $targetText = $target.text(),
+      $targetParent = $target.parent(),
+      $targetId = $target.attr('class');
+
+    if ($targetText === 'Methods:' || $targetText === 'Properties:') {
+      var $siblings = $targetParent.siblings();
+      if ($siblings.first().is(":hidden")) {
+        $siblings.slideDown();
+      } else {
+        $siblings.slideUp();
+      }
+    }
+
+    if ($targetId === 'mod-link') {
+      var $siblings = $targetParent.parent().siblings();
+      if ($siblings.first().is(":hidden")) {
+        $siblings.slideDown();
+      } else {
+        $siblings.slideUp();
+      }
+    }
+
+  });
+
+});
