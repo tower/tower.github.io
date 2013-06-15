85b2092a3aeea0da398fe6b6772704d1c4e33c6f
Lance Pollard <lancejpollard@gmail.com>
2013-06-14 20:12:21 -0700

    partials

--- app.js
+++ app.js
@@ -48,7 +48,13 @@ app.listen(process.env.PORT || 3000);
  */
 
 var directive = require('tower-directive');
+
+directive('data-content', function(scope, el, attr){
+  el.appendChild(scope.get('body')(scope));
+});
+
 var template = require('tower-template');
+var content = require('tower-content');
 var domify = require('domify');
 var jsdom = require('jsdom').jsdom;
 var fs = require('fs');
@@ -61,16 +67,26 @@ templates[layoutPath] = template(layout);
 // express 3.x template engine compliance
 
 function render(filename, options, cb) {
-  if (!templates[filename]) {
+  var startTime = new Date;
+  var fn = templates[filename];
+
+  if (!fn) {
     var html = fs.readFileSync(filename, 'utf-8');
     var el = domify(html, layout); // XXX: added second param, need to send PR
     //layout.appendChild(el);
     //console.log(layout.documentElement.innerHTML);
-    var fn = template(el);
+    templates[filename] = fn = template(el);
+  }
 
+  if (layoutPath === filename) {
+    var el = fn(content.root().update(options));
+    var endTime = new Date;
+    console.log(endTime - startTime);
+    //console.log(el.documentElement.innerHTML)
+    cb(null, el.documentElement.innerHTML);
+  } else {
+    var endTime = new Date;
+    console.log(endTime - startTime);
+    cb(null, fn); 
   }
 }
-
-directive('data-content', function(scope, el, attr){
-  console.log('content!');
-});
