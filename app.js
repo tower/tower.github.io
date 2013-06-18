b16502569da2123299eff95ce1eec711a2fbfce4
Lance Pollard <lancejpollard@gmail.com>
2013-06-18 13:56:40 -0700

    structure

--- app.js
+++ app.js
@@ -6,10 +6,14 @@
 var express = require('express');
 var partials = require('express-partials');
 var app = express();
+var template = require('tower-template');
+var content = require('tower-content');
 var router = require('tower-router');
 var route = require('tower-route');
-var path = require('path');
-var markdown = require('./markdown');
+var text = require('tower-text');
+var markdown = require('./lib/markdown');
+var guide = require('./lib/guide');
+var render = require('./lib/render');
 
 /**
  * Configuration.
@@ -24,6 +28,33 @@ app.configure(function(){
   app.use(router);
 });
 
+/**
+ * Guides.
+ */
+
+guide('resource');
+guide('adapter');
+guide('query');
+guide('template');
+guide('content');
+guide('directive');
+guide('element');
+guide('route');
+guide('cookbook');
+
+/**
+ * Content.
+ */
+
+content('body')
+  .helper('label', function(scope, name){
+    return text(name).render(scope);
+  });
+
+/**
+ * Routes.
+ */
+
 route('/', function(context){
   context.res.render('index');
   //context.res.send(document.innerHTML);
@@ -37,60 +68,8 @@ route('/api', function(context){
   context.res.render('docs');
 });
 
-app.get('/docs', function(req, res){
-  res.render('docs', { url: 'docs' });
-});
-
 /**
  * Listen.
  */
 
 app.listen(process.env.PORT || 3000);
-
-/**
- * tmp
- */
-
-var directive = require('tower-directive');
-
-directive('data-content', function(scope, el, attr){
-  el.appendChild(scope.get('body')(scope));
-});
-
-var template = require('tower-template');
-var content = require('tower-content');
-var domify = require('domify');
-var jsdom = require('jsdom').jsdom;
-var fs = require('fs');
-var path = require('path');
-var layoutPath = path.join(__dirname, 'views/layout.html');
-var layout = jsdom(fs.readFileSync(layoutPath));
-var templates = {};
-templates[layoutPath] = template(layout);
-
-// express 3.x template engine compliance
-
-function render(filename, options, cb) {
-  var startTime = new Date;
-  var fn = templates[filename];
-
-  if (!fn) {
-    var html = fs.readFileSync(filename, 'utf-8');
-    var el = domify(html, layout); // XXX: added second param, need to send PR
-    //layout.appendChild(el);
-    //console.log(layout.documentElement.innerHTML);
-    templates[filename] = fn = template(el);
-  }
-
-  if (layoutPath === filename) {
-    var el = fn(content.root().update(options));
-    var endTime = new Date;
-    console.log(endTime - startTime);
-    //console.log(el.documentElement.innerHTML)
-    cb(null, el.documentElement.innerHTML);
-  } else {
-    var endTime = new Date;
-    console.log(endTime - startTime);
-    cb(null, fn); 
-  }
-}
